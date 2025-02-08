97ae82df023b2a861c149aabc204652e0e6de609
vudkhoa <vudinhkhoaowl@gmail.com>
2025-02-08 12:54:05 +0700

    Using GetAxis() For Movement

---
+++ Assets/Player Controls.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalThrow = Input.GetAxis("Horizontal");
+        Debug.Log(horizontalThrow);
+
+        float verticalThrow = Input.GetAxis("Vertical");
+        Debug.Log(verticalThrow);
+    }
+}
